42a0568eddf0e1ca14c647ef1e69f0ee6f85fb23
leobarbosadev <leobarbosajf@hotmail.com>
2025-05-08 21:33:06 -0300

    Conclusão ex001(falta o CSS)

---
+++ exercicios/javascript/ex001/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let valor1 = document.getElementById('valor1');
+
+    let valor2 = document.getElementById('valor2');
+
+    let resultado = document.getElementById('resultado')
+    
+    // const btnSoma = document.getElementById('soma')
+    
+    // btnSoma.addEventListener('click', function(){
+    //     let calculo = parseFloat(valor1.value) + parseFloat(valor2.value)
+
+    //     resultado.textContent = calculo
+    // });
+
+    document.getElementById('soma').addEventListener('click', function(){
+        calculo = parseFloat(valor1.value) + parseFloat(valor2.value)
+        resultado.textContent = calculo
+    });
+
+    document.getElementById('subtracao').addEventListener('click', function(){
+        calculo = parseFloat(valor1.value) - parseFloat(valor2.value)
+        resultado.textContent = calculo
+    });
+
+    document.getElementById('divisao').addEventListener('click', function(){
+        if (parseFloat(valor2.value) == 0){
+
+            resultado.textContent = 'Não existe divisão por 0'
+
+        }else{
+            calculo = parseFloat(valor1.value) / parseFloat(valor2.value)
+        
+            resultado.textContent = calculo
+        }
+    });
+
+    document.getElementById('multiplicacao').addEventListener('click', function(){
+        calculo = parseFloat(valor1.value) * parseFloat(valor2.value)
+
+        resultado.textContent = calculo
+    });
+});
