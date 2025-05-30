1f504cf3ab159c0a7b7de664457f7c707246242e
leobarbosadev <leobarbosajf@hotmail.com>
2025-05-29 21:40:03 -0300

    Criação da pasta array

---
+++ aulas/javascript/aula004-array_percorrer_transformar/js/script.js
@@ -0,0 +1,21 @@
+/**
+ * ARQUIVO: metodo.js
+ * DESCRIÇÃO: Demonstração dos principais métodos de iteração de arrays de JavaScript
+ * AUTOR: Leonardo Barbosa
+ * DATA: 29/05/2025
+ * VERSÃO: 1.0
+ */
+
+//======================================================
+//DADOS DE EXEMPLO
+//======================================================
+
+//Array de números para exemplos básicos
+const numeros = [1, 2, 3, 4, 5, 6];
+
+//Array de objetos (pessoas) para exemplos mais complexos
+const pessoas = [
+    {nome: 'Gal Gadot', idade: 40, cidade: 'Rosh HaAyin'},
+    {nome: 'Elizabeth Olsen', idade: 36, cidade: 'Sherman Oaks'},
+    {nome: 'Alexandra Daddario', idade: 39, cidade:'Nova York'},
+]
