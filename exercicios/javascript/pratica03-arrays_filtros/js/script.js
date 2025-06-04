a41f8c85c6d2205fe8f25087bcba19c817bb10fd
Leonardo Barbosa <leobarbosajf@hotmail.com>
2025-06-04 16:20:11 -0300

    Funcionando filtro todos e alimentos sem CSS

--- exercicios/javascript/pratica03-arrays_filtros/js/script.js
+++ exercicios/javascript/pratica03-arrays_filtros/js/script.js
@@ -1,16 +1,43 @@
-const produtos = [
-    {nome: 'Sabonete Natural', preco: 15, categoria: 'cosmetico'},
-    {nome: 'Shampoo Orgânico', preco: 25, categoria: 'cosmetico'},
-    {nome: 'Granola Artesanal', preco: 18.50, categoria: 'alimento'},
-    {nome: 'Mel Puro', preco: 22, categoria: 'alimento'},
-];
+document.addEventListener('DOMContentLoaded', function(){
+    const produtos = [
+        {nome: 'Sabonete Natural', preco: 15, categoria: 'cosmeticos'},
+        {nome: 'Shampoo Orgânico', preco: 25, categoria: 'cosmeticos'},
+        {nome: 'Granola Artesanal', preco: 18.50, categoria: 'alimentos'},
+        {nome: 'Mel Puro', preco: 22, categoria: 'alimentos'}
+    ];
 
+    const teste = document.getElementById('produtos');
 
-// function mostrarItem(nome, valor){
+    const res = document.getElementById('resultado');
 
-// }
+    function mostrarTodos(){
+        let saida = '';
+            produtos.forEach(function(exibir){
+                saida += `${exibir.nome}<br> ${exibir.preco}<br>`;
+            });
+        res.innerHTML = `${saida}`;
+    }
+    
+    function mostrarAlimentos(){
+        const arrayFiltrado = produtos.filter(function(categoria){
+                return categoria.categoria === 'alimentos'
+            });
+            res.innerHTML = `${arrayFiltrado}`
 
-res = produtos.getElementById('produtos').value
+        let saida = '';
+        arrayFiltrado.forEach(function(exibir){
+            saida += `${exibir.nome}<br> ${exibir.preco}<br>`;
+        });
+    res.innerHTML = `${saida}`;
+    }
+    mostrarTodos();//Mostrar todos os produtos quando carregar a página a primeira vez
 
-document.getElementById('resultado').addEventListener('click', function(){
-})
+    teste.addEventListener('change', function(){
+        if(teste.value ==='todos'){
+            mostrarTodos();
+        }
+        else if(teste.value ==='alimentos'){
+            mostrarAlimentos();
+        }
+    });
+});
