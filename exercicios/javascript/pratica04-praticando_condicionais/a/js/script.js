959eeaf8069f7bf5c28cfb7bf2a0bcafbf13caac
Leonardo Barbosa <leobarbosajf@hotmail.com>
2025-06-24 16:28:07 -0300

    testando  o .value

--- exercicios/javascript/pratica04-praticando_condicionais/a/js/script.js
+++ exercicios/javascript/pratica04-praticando_condicionais/a/js/script.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
     const numero = document.getElementById('numero');
+    
+    // const numero = parseInt(document.getElementById('numero').value);
 
     const verificar = document.getElementById('verificar');
 
     const resultado = document.getElementById('resultado');
 
     function verificarParImpar(){
-        // if(isNaN(numero.value)){
+        if(isNaN(numero.value)){
+            resultado.textContent = 'Não é número'
+            return;
+        }
+
+        // if(isNaN(numero)){
         //     resultado.textContent = 'Não é número'
         //     return;
         // }
 
-        if(parseInt(numero.value) % 2 == 0){
+        else if(numero.value % 2 == 0){
             resultado.textContent = `O número ${numero.value} é par`
         }
         else{
