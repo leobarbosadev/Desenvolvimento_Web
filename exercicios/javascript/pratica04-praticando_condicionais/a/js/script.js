e7e4225bdedeb0f7704f77af01a2924a71140435
leobarbosadev <leobarbosajf@hotmail.com>
2025-06-23 21:43:10 -0300

    criação pastas exercicio a

---
+++ exercicios/javascript/pratica04-praticando_condicionais/a/js/script.js
@@ -0,0 +1,3 @@
+// A empresa WebData Solutions contratou você para desenvolver uma página interativa que ajude seus usuários a classificar números.
+// Eles precisam de uma interface onde o usuário insira um número inteiro, clique em um botão e receba na tela a informação se o número é par ou ímpar.
+// Esse recurso será integrado ao site da empresa para pequenos cálculos rápidos.
