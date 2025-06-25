f407101777235eb97109404dffdf3f3428724fbe
leobarbosadev <leobarbosajf@hotmail.com>
2025-06-24 21:41:46 -0300

    Finalizando a atividade d

--- exercicios/javascript/pratica04-praticando_condicionais/atividade_b/js/script.js
+++ exercicios/javascript/pratica04-praticando_condicionais/atividade_b/js/script.js
@@ -1,20 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const num1 = document.getElementById('numero1');
-
-    const num2 = document.getElementById('numero2');
-
-    const num3 = document.getElementById('numero3');
-
+    
     const verificar = document.getElementById('verificar');
-
+    
     const resultado = document.getElementById('resultado')
-
+    
     function verificarIgualdade(){
-        //SE PASSAR AQUI PRIMEIRO, NÃO ENTRA NO OUTRO IF
-        // if (isNaN(num1) || isNaN(num2) || isNaN(num3)){
-        //     resultado.textContent = 'Não é numero'
-        //     return;
-        // }
+        const num1 = document.getElementById('numero1');
+    
+        const num2 = document.getElementById('numero2');
+    
+        const num3 = document.getElementById('numero3');
 
         if(num1.value == num2.value == num3.value){
             resultado.textContent = 'Os números são iguais'
--- exercicios/javascript/pratica04-praticando_condicionais/atividade_d/js/script.js
+++ exercicios/javascript/pratica04-praticando_condicionais/atividade_d/js/script.js
@@ -1,3 +1,33 @@
+// Se o salário for menor que R$1000,00, aplica-se um aumento de 10%.
+// Se o salário for maior que R$1500,00, aplica-se um aumento de 5%.
+// Se o salário for entre R$1000,00 e R$1500,00 (inclusive), o salário permanece o mesmo.
+// O sistema também deve verificar se o valor informado não é zero ou negativo e, nesse caso, exibir uma mensagem de erro.
+
+
 document.addEventListener('DOMContentLoaded', function(){
     
+    
+    const verificar = document.getElementById('verificar');
+
+    const resultado = document.getElementById('resultado');
+    
+    function calcularSalario(){
+        let salario = parseFloat(document.getElementById('salario').value);
+        
+        let novo_salario = 0;
+
+        if(salario < 1000){
+            novo_salario = salario + (salario * 0.1);
+            resultado.textContent = `Seu salário atual é de R$${salario.toFixed(2)}, seu novo salário com 10% de acréscimo é de R$${novo_salario.toFixed(2)}`;
+        }
+        else if(salario > 1500){
+            novo_salario = salario + (salario * 0.05);
+            resultado.textContent = novo_salario
+        }
+
+    }
+
+    verificar.addEventListener('click', function(){
+        calcularSalario()
+    });
 })
