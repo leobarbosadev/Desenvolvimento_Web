3d83223e0c9c1832223256f6a7c6c4c92ca8540c
leobarbosadev <leobarbosajf@hotmail.com>
2025-06-26 19:43:20 -0300

    Validando o valor de a na atividade h

--- exercicios/javascript/pratica04-praticando_condicionais/atividade_h/js/script.js
+++ exercicios/javascript/pratica04-praticando_condicionais/atividade_h/js/script.js
@@ -12,17 +12,26 @@ document.addEventListener('DOMContentLoaded', function(){
 
         let delta = parseFloat((valorb**2) - 4*valora*valorc)
 
-        let raiz_delta = Math.sqrt(delta)
+        let raiz_delta = delta **(1/2);
+        // let raiz_delta = Math.sqrt(delta)
 
         if(delta < 0){
             resultado.textContent = `O valor de \u0394 é ${delta}, não existe raiz real`
         }
         else{
-            x1 = (-valorb + raiz_delta)/ (2 * valora)
+            if(valora === 0){
+                resultado.textContent = 'ATENÇÃO!!! Valor de a não pode ser 0'
 
-            x2 = (-valorb -raiz_delta)/ (2 * valora)
+            }else{
+                
+                x1 = (-valorb + raiz_delta)/ (2 * valora)
+    
+                x2 = (-valorb -raiz_delta)/ (2 * valora)
+                
+                resultado.textContent = `O valor de \u0394 é ${delta}, os valores das raizes serão iguais a: x'= ${x1} e x''= ${x2}` // \u0394 É O SÍMBOLO DE DELTA EM UNICODE
+            }
+            //VALIDAR O VALOR DE a NÃO PODE SER 0
 
-            resultado.textContent = `O valor de \u0394 é ${delta}, os valores das raizes serão iguais a: x'= ${x1} e x''= ${x2}` // \u0394 É O SÍMBOLO DE DELTA EM UNICODE
         }
     }
 
