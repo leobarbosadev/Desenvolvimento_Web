fdbef63e8efb6a5e0488b38f6a3064830907c6cc
Leonardo Barbosa <leobarbosajf@hotmail.com>
2025-06-26 12:27:46 -0300

    finalizando atividade H

---
+++ exercicios/javascript/pratica04-praticando_condicionais/atividade_h/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const resultado = document.getElementById('resultado');
+
+    const verificar = document.getElementById('verificar');
+
+    function calcularRaiz(){
+        const valora = parseFloat(document.getElementById('valora').value);
+
+        const valorb = parseFloat(document.getElementById('valorb').value);
+
+        const valorc = parseFloat(document.getElementById('valorc').value);
+
+        let delta = parseFloat((valorb**2) - 4*valora*valorc)
+
+        let raiz_delta = Math.sqrt(delta)
+
+        if(delta < 0){
+            resultado.textContent = `O valor de \u0394 é ${delta}, não existe raiz real`
+        }
+        else{
+            x1 = (-valorb + raiz_delta)/ (2 * valora)
+
+            x2 = (-valorb -raiz_delta)/ (2 * valora)
+
+            resultado.textContent = `O valor de \u0394 é ${delta}, os valores das raizes serão iguais a: x'= ${x1} e x''= ${x2}` // \u0394 É O SÍMBOLO DE DELTA EM UNICODE
+        }
+    }
+
+    verificar.addEventListener('click', function(){
+        calcularRaiz()
+    })
+})
