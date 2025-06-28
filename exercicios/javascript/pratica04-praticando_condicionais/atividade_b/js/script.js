73a6c84246193efa3c62943c237677ec026812d9
leobarbosadev <leobarbosajf@hotmail.com>
2025-06-27 21:27:06 -0300

    atualizadando a atividade b

--- exercicios/javascript/pratica04-praticando_condicionais/atividade_b/js/script.js
+++ exercicios/javascript/pratica04-praticando_condicionais/atividade_b/js/script.js
@@ -5,18 +5,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const resultado = document.getElementById('resultado')
     
     function verificarIgualdade(){
-        const num1 = document.getElementById('numero1');
+        const num1 = parseFloat(document.getElementById('numero1').value);
     
-        const num2 = document.getElementById('numero2');
+        const num2 = parseFloat(document.getElementById('numero2').value);
     
-        const num3 = document.getElementById('numero3');
+        const num3 = parseFloat(document.getElementById('numero3').value);
 
-        if(num1.value == num2.value == num3.value){
+        if(num1 == num2 == num3){
             resultado.textContent = 'Os números são iguais'
         }
         else{
-            const maior = Math.max(num1.value, num2.value, num3.value)
-            const menor = Math.min(num1.value, num2.value, num3.value)
+            const maior = Math.max(num1, num2, num3)
+            const menor = Math.min(num1, num2, num3)
             
             resultado.textContent = `O maior número é ${maior} e o menor é ${menor}`
         }
